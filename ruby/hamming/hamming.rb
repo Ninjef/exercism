0a85d6fffd223a37cdea5eddf92e475549343d01
Ninjef <jeff.arnold@welltok.com>
2016-04-14 12:57:12 -0600

    hamming change

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -2,12 +2,9 @@ class Hamming
   VERSION = 1
   def self.compute(strand1, strand2)
     raise ArgumentError if strand1.length != strand2.length
-    difference = 0
-    strand1.each_char.with_index do | char, index |
-      if strand2[index] != char
-        difference += 1
-      end
-    end
-    return difference
+    strand2_arr = strand2.chars
+    strand1.chars.delete_if.with_index do | char, index |
+      char == strand2_arr[index]
+    end.length
   end
 end
