d71d2c3aa3f615813f04432c27f14e3b085592bb
Ninjef <jeff.arnold@welltok.com>
2016-08-26 08:40:43 -0600

    robot name

---
+++ ruby/robot-name/robot_name.rb
@@ -0,0 +1,25 @@
+module BookKeeping
+  VERSION = 2
+end
+
+class Robot
+  attr_reader :name
+  
+  @@taken_names = []
+
+  def initialize
+    generate_name
+  end
+  def reset
+    generate_name
+  end
+
+  private
+
+  def generate_name
+    begin
+      @name = (0..1).map { (65 + rand(25)).chr }.join << (100 + rand(899)).to_s
+    end until !@@taken_names.include?(@name)
+    @@taken_names << @name
+  end
+end
